4fb2f8937e5015ac227c58a754eea2d568557875
Muzaffar <muzaffarshaikh517@gmail.com>
2020-06-21 23:45:04 -0600

    Updated Hello World C# console application with citation

--- HelloWorldUserEB/Test_HelloWorld/UnitTest1.cs
+++ HelloWorldUserEB/Test_HelloWorld/UnitTest1.cs
@@ -20,6 +20,11 @@ namespace Test_HelloWorld
         {
             Assert.Pass();
         }
+
+        //Citation:
+        //https://docs.google.com/document/d/1n6xVp-L6HJCccjl3KqCAkLAqnWtxM6AXj2PSPkZ0JTQ/edit
+        //The following unit test will check the console output inside SW variable
+        //We will compare the value of SW with our expected result to make sure it is asserted
         public void Test_HelloWorld()
         {
             String expected = "Hello World!";
@@ -29,5 +34,6 @@ namespace Test_HelloWorld
             var result = sw.ToString().Trim();
             Assert.AreEqual(expected, result);
         }
+        //End Citation
     }
 }
